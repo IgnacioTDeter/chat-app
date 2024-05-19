aa5e1bb88b106669878b84dc4c5198e3bb3a4fcc
IgnacioTDeter <detergithub@gmail.com>
2024-05-19 14:34:47 -0300

    Creation of user.model
    Filling the try-catch of signup(auth.controllers)

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,19 +1,34 @@
+import User from '../models/user.model.js';
 
 
-export const login = async (req, res) => {
+export const login = (req, res) => {
     console.log("LoginUser");
     res.send("Login successful");
 
+};
+
+export const signup = async (req, res) => {
+    console.log("signupUser");
+    res.send("Signup successful");
+
     try {
         const {fullName, userName, password, confirmPassword, gender} = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({error: "Passwords do not match"})     
+        }
+
+        const user = await User.findOne({username})
+
+        if(user){
+            return res.status(400).json({error: "This username already exist"})
+        }
+
+        //HASH PASSWORD HERE 
+        
     } catch (error) {
         
     }
-};
-
-export const signup = (req, res) => {
-    console.log("signupUser");
-    res.send("Signup successful");
 };
 
 export const logout = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,19 @@ import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js'
 import authRoutes from './routes/auth.routes.js'
 
-dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+dotenv.config();
 
 app.listen(PORT, () => {
     connectMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
+
+
+app.use(express.json()) // Middleware para analizar el JSON en el cuerpo de las solicitudes
 
 app.use("/api/auth", authRoutes)
 
