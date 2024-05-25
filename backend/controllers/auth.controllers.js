15089b420201c9c1edc99ea89094df8ed2cd7f68
IgnacioTDeter <detergithub@gmail.com>
2024-05-25 19:46:25 -0300

    Login controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -3,11 +3,7 @@ import bycript from 'bcryptjs';
 import generateTokenAndSetCookie from '../utils/generate.token.js';
 
 
-export const login = (req, res) => {
-    console.log("LoginUser");
-    res.send("Login successful");
 
-};
 
 export const signup = async (req, res) => {
     console.log(req.body)
@@ -56,6 +52,32 @@ export const signup = async (req, res) => {
 		}
 	} catch (error) {
 		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const login = async (req, res) => {
+
+	try {
+		const {username, password} = req.body;
+		const user = await User.findOne({username});
+		const isPasswordCorrect = await bycript.compare(password, user?.password || "")
+
+		if(!user || !isPasswordCorrect){
+			return res.status(400).json({error: "Invalid username or password"})
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
+
+
+	} catch (error) {
+		console.log("Error in login controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
