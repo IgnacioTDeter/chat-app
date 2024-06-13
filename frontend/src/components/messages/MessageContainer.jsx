429279d6264e0e4be4d5ec883ebc9573f2540849
IgnacioTDeter <detergithub@gmail.com>
2024-06-13 18:48:24 -0300

    Finish Topchat & setup Keyboard

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,10 +1,11 @@
 import TopChat from "./TopChat"
+import Keyboard from "./Keyboard.jsx"
 
 const MessageContainer = () =>{
     return(
         <section className="w-full rounded-tr-md rounded-br-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 ">
             <TopChat/>
-
+            <Keyboard/>
         </section>
     )
 }
--- frontend/src/components/messages/TopChat.jsx
+++ frontend/src/components/messages/TopChat.jsx
@@ -1,8 +1,25 @@
-const TopChat = () =>{
-    return(
-        <article className="w-full h-16 bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50">
-            d
-        </article>
+const TopChat = () => {
+    return (
+        <article className="flex w-full h-20 bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-tr-md p-2">
+
+            <div className="flex items-center gap-4">
+                <div className="avatar online flex items-center">
+                    <div className="ml-4 max-w-14 min-w-14 rounded-full">
+                        <img src="/chicken.png" alt="user avatar" />
+                    </div>
+                </div>
+                <div className="flex flex-col justify-between">
+                    <div className="font-bold text-white">
+                        <p>Jhon Doe</p>
+                    </div>
+                    <div className="text-gray-500 text-xs  ">
+                    Having coffee ☕ and procrastinating a bit
+                    </div>
+                </div>
+            </div>
+
+
+        </article >
     )
 }
 
