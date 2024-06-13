0cf8c69aefea00e8e13530dfb7dc128451841809
IgnacioTDeter <detergithub@gmail.com>
2024-06-13 19:30:52 -0300

    Finish Keyboard

--- frontend/src/components/messages/Keyboard.jsx
+++ frontend/src/components/messages/Keyboard.jsx
@@ -1,8 +1,29 @@
-const Keyboard = () =>{
-    return(
-        <div>
+import { CiImageOn } from "react-icons/ci";
+import { MdEmojiEmotions } from "react-icons/md";
 
-        </div>
+
+const Keyboard = () =>{
+
+
+    return(
+        <article className="flex w-full h-16 bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 rounded-br-md p-2 mt-auto fixed bottom-0">
+
+            <div className="text-white p-1 mr-1 ">
+                <CiImageOn size={40}/>
+            </div>
+            <div className="text-white p-1 mr-2">
+                <MdEmojiEmotions size={40}/>
+            </div>
+            <input 
+                className="w-2/3 text-gray-200 focus:outline-none bg-slate-600 rounded-md flex p-3"
+                type="text"
+                placeholder="Type a message"
+            />
+
+            <button className="bg-amber-500 hover:bg-amber-600 transition-colors ml-2 w-1/5 rounded-md">
+                Enviar
+            </button>
+        </article>
     )
 }
 
