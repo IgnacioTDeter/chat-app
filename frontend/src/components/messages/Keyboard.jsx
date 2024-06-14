9610f9cb94923b3d3b153793b8cae38a38b26bf9
IgnacioTDeter <detergithub@gmail.com>
2024-06-14 17:16:41 -0300

    Making article a form

--- frontend/src/components/messages/Keyboard.jsx
+++ frontend/src/components/messages/Keyboard.jsx
@@ -6,7 +6,7 @@ const Keyboard = () =>{
 
 
     return(
-        <article className="flex w-full h-16 bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 rounded-br-md p-2 mt-auto fixed bottom-0">
+        <form className="flex w-full h-16 bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 rounded-br-md p-2 mt-auto fixed bottom-0">
 
             <div className="text-white p-1 mr-1 cursor-pointer">
                 <CiImageOn size={40}/>
@@ -20,10 +20,10 @@ const Keyboard = () =>{
                 placeholder="Type a message"
             />
 
-            <button className="bg-amber-500 hover:bg-amber-600 transition-colors ml-2 w-1/5 rounded-md font-bold" alt="Enviar">
+            <button type="submit" className="bg-amber-500 hover:bg-amber-600 transition-colors ml-2 w-1/5 rounded-md font-bold" alt="Enviar">
                 Enviar
             </button>
-        </article>
+        </form>
     )
 }
 
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,6 +3,8 @@ import Keyboard from "./Keyboard.jsx"
 import Chat from "./Chat.jsx"
 
 const MessageContainer = () =>{
+
+
     return(
         <section className="w-full  rounded-tr-md rounded-br-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 flex-col ">
             <TopChat/>
