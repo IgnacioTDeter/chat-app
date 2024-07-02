24d893d2b52b17aad4e87098f25fdacddf55702c
IgnacioTDeter <detergithub@gmail.com>
2024-07-02 19:50:15 -0300

    Comentando Message.controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -5,42 +5,44 @@
 	export const sendMessage = async (req, res) => {
 		try {
 			const { message } = req.body;
-			const { id: receiverId } = req.params;
-			const senderId = req.user._id;
+			const { id: receiverId } = req.params; // Obtiene el ID del receptor de los parámetros de la solicitud
+			const senderId = req.user._id; // Obtiene el ID del remitente desde el usuario autenticado en la solicitud
 
+			// Busca una conversación existente entre el remitente y el receptor
 			let conversation = await Conversation.findOne({
 				participants: { $all: [senderId, receiverId] },
 			});
 
+			// Si no existe la conversación, crea una nueva
 			if (!conversation) {
 				conversation = await Conversation.create({
 					participants: [senderId, receiverId],
 				});
 			}
 
+			// Crea un nuevo mensaje
 			const newMessage = new Message({
 				senderId,
 				receiverId,
 				message,
 			});
 
+			// Añade el ID del nuevo mensaje a la lista de mensajes de la conversación
 			if (newMessage) {
 				conversation.messages.push(newMessage._id);
 			}
 
-			// await conversation.save();
-			// await newMessage.save();
-
-			// this will run in parallel
+			// Guarda la conversación y el mensaje de manera simultánea usando Promise.all
 			await Promise.all([conversation.save(), newMessage.save()]);
 
 			// SOCKET IO FUNCTIONALITY WILL GO HERE
-			const receiverSocketId = getReceiverSocketId(receiverId);
+			const receiverSocketId = getReceiverSocketId(receiverId); // Obtiene el ID de socket del receptor
 			if (receiverSocketId) {
-				// io.to(<socket_id>).emit() used to send events to specific client
+				// Emite un evento "newMessage" al cliente específico usando su ID de socket
 				io.to(receiverSocketId).emit("newMessage", newMessage);
 			}
 
+			// Devuelve el mensaje creado como respuesta
 			res.status(201).json(newMessage);
 		} catch (error) {
 			console.log("Error in sendMessage controller: ", error.message);
@@ -50,15 +52,18 @@
 
 	export const getMessages = async (req, res) => {
 		try {
-			const { id: userToChatId } = req.params;
-			const senderId = req.user._id;
+			const { id: userToChatId } = req.params; // Obtiene el ID del usuario con el que se está chateando desde los parámetros de la solicitud
+			const senderId = req.user._id; // Obtiene el ID del remitente desde el usuario autenticado en la solicitud
 
+
+			// Busca la conversación entre el remitente y el usuario con el que se está chateando
 			const conversation = await Conversation.findOne({
 				participants: { $all: [senderId, userToChatId] },
-			}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+			}).populate("messages"); // Popula los mensajes para obtener los documentos completos de los mensajes
 
 			if (!conversation) return res.status(200).json([]);
 
+			// Obtiene los mensajes de la conversación encontrada
 			const messages = conversation.messages;
 
 			res.status(200).json(messages);
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,16 +1,18 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
+	// Genera un token JWT firmado con userId como payload y JWT_SECRET como clave secreta
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	});
 
 	console.log("Generated Token: ", token); // Add this line for debugging
 
+	// Configura una cookie en la respuesta (res) con el token JWT
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		httpOnly: true, // La cookie solo es accesible para HTTP, previene ataques XSS (cross-site scripting)
+		sameSite: "strict", // Restringe el envío de cookies en solicitudes cross-site para prevenir ataques CSRF (cross-site request forgery)
 		secure: process.env.NODE_ENV !== "development",
 	});
 };
