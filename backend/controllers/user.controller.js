d0fa8eaf483871f16212c3c7d2ed0fda16785981
IgnacioTDeter <detergithub@gmail.com>
2024-07-02 19:53:56 -0300

    Comentando user.controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,8 +2,9 @@ import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
 	try {
-		const loggedInUserId = req.user._id;
-
+		const loggedInUserId = req.user._id; // Obtiene el ID del usuario autenticado
+		
+		// Busca todos los usuarios excepto el usuario autenticado actual, excluyendo su contraseña
 		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
 		res.status(200).json(filteredUsers);
