deebd03d25079aaa05eddc20a1d4b1e813a9a3ad
IgnacioTDeter <detergithub@gmail.com>
2024-07-02 09:04:40 -0300

    Search Users
    Ahora funciona el buscador de usuarios

--- frontend/src/components/messages/Chat.jsx
+++ frontend/src/components/messages/Chat.jsx
@@ -4,7 +4,7 @@ import Message from "./Message";
 
 const Chat = () => {
     const { messages, loading } = useGetMessages();
-    console.log("messages:", messages);
+    
     return (
         <article className="px-4 flex-1 h-[405px] overflow-auto">
             {!loading && messages.length > 0 && messages.map(message => (
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations"
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+
+    const [search, setSearch] = useState("")
+
+    const { setSelectedConversation } = useConversation();
+    const {conversations} = useGetConversations()
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if(!search)return
+        
+        const conversation = conversations.find(
+            (c) => c.fullName.toLowerCase().includes(search.toLowerCase())
+        )
+
+        if(conversation){
+            setSelectedConversation(conversation);
+            setSearch('');
+        } else {
+            toast.error("No se encontro ningun usuario")
+        }
+    }
+
     return (
-        <form className="flex items-center justify-center gap-2 w-full mt-2 mb-7 px-4" action="">
-            <input type="" className="input w-full h-10 items-center " placeholder="Buscar..." />
+        <form onSubmit={handleSubmit} className="flex items-center justify-center gap-2 w-full mt-2 mb-7 px-4" action="">
+            <input type="" className="input w-full h-10 items-center " placeholder="Buscar..." 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            />
 
             <button className="bg-amber-500 hover:bg-amber-600 transition-colors p-2 w-24 h-10 rounded-md flex justify-center items-center">
             <IoSearch className="" />
